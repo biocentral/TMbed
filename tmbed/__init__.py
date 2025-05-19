c2db20b33ff4c91718d7dfcac6c7c25c365a678d
Sebastian <sebastian.franz@tum.de>
2025-05-19 14:55:44 +0200

    Adding Decoder to module API

--- tmbed/__init__.py
+++ tmbed/__init__.py
@@ -12,5 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .viterbi import Decoder
+
+__all__ = ["Decoder"]
 
 __version__ = '1.0.2'
